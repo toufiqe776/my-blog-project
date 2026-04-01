39ff0db6b0ee161eb11635f0bfe136df77a0de60
toufiqe776 <toufiq.nt@gmail.com>
2026-04-02 02:07:43 +0530

    adding list  of blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import NotFound from './components/NotFound'
+import BlogsList from './components/BlogsList'
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<BlogsList />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="*" element={<NotFound />} />
